9922eb235bda35f774d819685ab670ffca5b7fe0
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-10-07 19:16:06 -0400

    MongoDB Atlas Setup Completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://thiago:thiago123@mongocluster.aoylqre.mongodb.net/media2?retryWrites=true&w=majority"

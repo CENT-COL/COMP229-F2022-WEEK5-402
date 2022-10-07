586c966d542acd4e208be0fb704d427d2d343159
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-10-07 19:45:18 -0400

    Passport Setup Completed + Login and Registration EJS Pages

--- app/app.js
+++ app/app.js
@@ -9,6 +9,17 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Auth Step 1 - import modules
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import flash from 'connect-flash';
+
+// Auth Step 2 - define our auth strategy
+let localStrategy = passportLocal.Strategy;
+
+// Auth Step 3 - import the user model
+import User from './models/user.js';
+
 // Import Mongoose Module
 import mongoose from 'mongoose';
 
@@ -42,11 +53,28 @@ app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname,'../public')));
+
+
+// Auth Step 4 - Setup Express Session
 app.use(session({
     secret: Secret,
     saveUninitialized: false, 
     resave: false
 }));
+
+// Auth Step5 - Setup Flash
+app.use(flash());
+
+// Auth Step 6 - Initialize Passport and Session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Auth Step 7 - Implement the Auth Strategy
+passport.use(User.createStrategy());
+
+// Auth Step 8 - Setup serialization and deserialization
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Use Routes
 app.use('/', indexRouter);
---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
+const { PassportLocalSchema } = mongoose;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    displayName: String,
+    username: String,
+    emailAddress: String,    
+}, {
+    timestamps: true,
+    colleciton: 'users'
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+export default mongoose.model('User', UserSchema);

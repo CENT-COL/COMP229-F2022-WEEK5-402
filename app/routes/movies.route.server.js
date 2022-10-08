c5428a0fb1ebd0d93735f9c1c9759c9cf9881404
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-10-07 20:57:48 -0400

    Authorization Completed

--- app/routes/movies.route.server.js
+++ app/routes/movies.route.server.js
@@ -7,13 +7,15 @@ import {  DisplayMoviesList,
     DisplayMoviesEditPage, 
     ProcessMoviesDelete } from "../controllers/movies.controller.server.js";
 
+import { AuthGuard } from "../utils/index.js";
+
 const router = Router();
 
 router.get('/movie-list', DisplayMoviesList);
-router.get('/movie-add', DisplayMoviesAddPage);
-router.post('/movie-add', ProcessMoviesAddPage);
-router.post('/movie-edit/:id', ProcessMoviesEditPage);
-router.get('/movie-edit/:id', DisplayMoviesEditPage);
-router.get('/movie-delete/:id', ProcessMoviesDelete);
+router.get('/movie-add', AuthGuard, DisplayMoviesAddPage);
+router.post('/movie-add', AuthGuard,ProcessMoviesAddPage);
+router.post('/movie-edit/:id', AuthGuard,ProcessMoviesEditPage);
+router.get('/movie-edit/:id', AuthGuard,DisplayMoviesEditPage);
+router.get('/movie-delete/:id', AuthGuard,ProcessMoviesDelete);
 
 export default router;

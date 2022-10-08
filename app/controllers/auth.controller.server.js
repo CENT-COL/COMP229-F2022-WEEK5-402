fc579a413c92d44d91a3b4cb3b8b9083ae2fce50
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-10-07 20:32:11 -0400

    Authentication Completed

--- app/controllers/auth.controller.server.js
+++ app/controllers/auth.controller.server.js
@@ -21,3 +21,69 @@ export function DisplayRegisterPage(req, res, next){
         return res.render('index', {title: 'Register', page: 'register', messages: req.flash('registerMessage') });
     }
 }
+
+// Process Functions
+export function ProcessLoginPage(req, res, next){
+    passport.authenticate('local', function(err, user, info){
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        if(!user){
+            req.flash('loginMessage', 'Authetication Error');
+            return res.redirect('/login');
+        }
+
+        req.logIn(user, function(err){
+            if(err){
+                console.error(err);
+                res.end(err);
+            }
+
+            return res.redirect('/')
+        });
+
+
+    })(req, res, next);
+}
+
+export function ProcessRegisterPage(req, res, next){
+    let newUser = new User({
+        username: req.body.username,
+        emailAddress: req.body.emailAddress,
+        displayName: req.body.firstName + " " + req.body.lastName
+    });
+
+    User.register(newUser, req.body.password, function(err){
+        if(err){
+            if(err.name == "UserExistsError"){
+                console.error('Error: User Already Exists');
+                req.flash('registerMessage', 'User Already Exits');
+            } else {
+                console.error(err);
+                req.flash('registerMessage', 'Server Error');
+            }
+
+            return res.redirect('/register');
+        }
+
+        return passport.authenticate('local')(req, res, function(){
+            return res.redirect('/');
+        });
+    })
+}
+
+export function ProcessLougoutPage(req, res, next){
+    req.logOut(function(err){
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        console.log('user logged out successfully');
+    });
+
+    res.redirect('/login');
+}
+
